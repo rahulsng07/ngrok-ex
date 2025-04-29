a26cc286e16b52d7d50c552e67a186586c8b6e52
Rahul Singh <rahulsingh875724@gmail.com>
2025-04-29 09:28:38 +0530

    Update MyController.java

--- springboot-ngrok-testing/src/main/java/com/ngrok/springboot_ngrok_testing/MyController.java
+++ springboot-ngrok-testing/src/main/java/com/ngrok/springboot_ngrok_testing/MyController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 	// hello this is test of jenkins only
+	// hello jenkins
 public class MyController {
 	@Autowired
 	@RequestMapping("/test")
